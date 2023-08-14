56a27f85205cc2787b9fd12e96eab68a259e4594
dayanasilvacaffarel <dayana.silva.caffarel@gmail.com>
2023-08-14 00:20:06 -0300

    Segundo commit

--- src/main/java/com/sistemavotacion/demo/model/Voto.java
+++ src/main/java/com/sistemavotacion/demo/model/Voto.java
@@ -19,4 +19,39 @@ public class Voto {
     @ManyToOne
     @JoinColumn(name = "votante_id", referencedColumnName = "id")
     private Votante votante;
+
+    public Voto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Candidato getCandidato() {
+        return candidato;
+    }
+
+    public void setCandidato(Candidato candidato) {
+        this.candidato = candidato;
+    }
+
+    public Votante getVotante() {
+        return votante;
+    }
+
+    public void setVotante(Votante votante) {
+        this.votante = votante;
+    }
 }
--- src/main/java/com/sistemavotacion/demo/service/VotoService.java
+++ src/main/java/com/sistemavotacion/demo/service/VotoService.java
@@ -1,15 +1,37 @@
 package com.sistemavotacion.demo.service;
 
+import com.sistemavotacion.demo.model.Candidato;
+import com.sistemavotacion.demo.model.Votante;
 import com.sistemavotacion.demo.model.Voto;
+import com.sistemavotacion.demo.repository.CandidatoRepository;
+import com.sistemavotacion.demo.repository.VotanteRepository;
 import com.sistemavotacion.demo.repository.VotoRepository;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 @Service
 public class VotoService {
 
     private VotoRepository votoRepository;
+    private VotanteRepository votanteRepository;
+    private CandidatoRepository candidatoRepository;
 
+    public void registrarVoto (Long id){
+        Votante votante = votanteRepository.findById(id).orElse(null);
+        Candidato candidato = candidatoRepository.findById(id).orElse(null);
+
+        if(votante==null||candidato==null){
+            return;
+        }
+        Voto voto = new Voto();
+        voto.setVotante(votante);
+        voto.setCandidato(candidato);
+        voto.setFecha(LocalDate.now());
+        votoRepository.save(voto);
+
+    }
     public void guardar(Voto voto) {
         votoRepository.save(voto);
     }
